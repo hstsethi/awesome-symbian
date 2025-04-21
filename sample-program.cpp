e7b6f523d0e2cb8cb4b1b89a2f0664c461903017
HstSethi <hstsethi@outlook.com>
2025-04-21 09:22:57 +0530

    add a sample program for Symbian in C++

---
+++ sample-program.cpp
@@ -0,0 +1,9 @@
+#include "CommonFramework.h"
+
+// This is just a sample code to register this repository as a C++ repository for SEO and curating purposes. Hence the other header, and build files are not included. The main content is all in readme.
+
+LOCAL_C void doExampleL()
+    {
+	_LIT(KHelloWorldText,"Welcome to Awesome Symbian!\n"); // Prints hello world to console
+	console->Printf(KHelloWorldText);
+	}
